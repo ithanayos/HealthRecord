2fbf71a137ac9fa1f909c17359ec5a594b7dc31e
ithanayos <ithanayos@gmail.com>
2015-10-09 13:48:41 +0700

    Create AlertDialog

--- app/src/main/java/utcc/som/cken/tae/healthrecord/SignUpActivity.java
+++ app/src/main/java/utcc/som/cken/tae/healthrecord/SignUpActivity.java
@@ -2,6 +2,7 @@ package utcc.som.cken.tae.healthrecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+    } // OnCreate
+
+    public void clickSave(View view){
+
+
+
+
+
+    } // clickSave
+
+
+
+} //Main Class
