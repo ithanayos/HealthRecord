81cdf96d9dd53efc2ee6343d85d19da890cab2b8
ithanayos <ithanayos@gmail.com>
2015-10-09 14:19:40 +0700

    Design RecordActivity Success

--- app/src/main/java/utcc/som/cken/tae/healthrecord/SignUpActivity.java
+++ app/src/main/java/utcc/som/cken/tae/healthrecord/SignUpActivity.java
@@ -3,14 +3,34 @@ package utcc.som.cken.tae.healthrecord;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText, nameEditText,
+            emailEditText, weightEditText, heightEditText; // ใช้ในการกรอก
+    private RadioGroup sexRadioGroup;
+    private Spinner ageSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Bind Widget
+
+        bindWidget();
+
     } // OnCreate
+
+    private void bindWidget() {
+
+
+    }
 
     public void clickSave(View view){
 
