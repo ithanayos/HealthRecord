4d0ee1a49b2d17db1939fba0e64899f58a46a3a2
ithanayos <ithanayos@gmail.com>
2015-10-11 13:35:39 +0700

    SynJSONtoSQLite and DeleteAllData Success

--- app/src/main/java/utcc/som/cken/tae/healthrecord/MainActivity.java
+++ app/src/main/java/utcc/som/cken/tae/healthrecord/MainActivity.java
@@ -1,16 +1,33 @@
 package utcc.som.cken.tae.healthrecord;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private UserTABLE objUserTABLE;
 
-
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +38,172 @@ public class MainActivity extends AppCompatActivity {
         createDatabase();
 
         //Test Add Value SQLite
-        testAddValue();
+        //testAddValue();
+
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
+
+        bindWidget();
+
+        //deleteAllData();
 
     } //OnCreate
 
+    private void deleteAllData() {
+
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase
+                ("Health.db", MODE_APPEND, null);
+        objSqLiteDatabase.delete("userTABLE", null, null);
+
+    } // deleteAllData
+
+    private void bindWidget() {
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPass);
+
+
+    }
+
+    private void synJSONtoSQLite() {
+
+// Step ในการทำการ Synchronize ข้อมูลจาก JSON To SQLite
+        // ข้อที่ 0.  Change Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+
+        // Sync ข้อมูลจาก Table โดยค่อยๆทำทีละ Table
+        int intTimes = 0;    // จำนวนครั้ง
+        while (intTimes <= 1) {
+
+
+            // Constant
+            InputStream objInputStream = null;  // โหลดไปใช้ไป
+            String strJSON = null;  // จะเปลี่ยน Input Stream ให้เป็น String
+            String strUrlUser = "http://swiftcodingthai.com/tae/get_data_user_tae.php";   // URL ของไฟล์ JSON ตาราง User
+            HttpPost objHttpPost = null;   // ประกาศตัวแปรไว้
+
+            // ข้อที่ 1. Create InputStream   ทำให้มันโหลดแบบ Streaming ให้ได้ก่อน
+            try {   // สิ่งที่เสี่ยงต่อการ Error ใส่ในนี้
+
+                HttpClient objHttpClient = new DefaultHttpClient();
+                switch (intTimes) {
+                    case 0:
+                        objHttpPost = new HttpPost(strUrlUser);
+                        break;
+                }
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+
+            } catch (Exception e) { // ถ้า Error จะเข้ามาในนี้
+
+                Log.d("Rest", "InputStream ==>" + e.toString());
+
+            }
+
+
+            // ข้อที่ 2. Create strJSON     เปลี่ยนสิ่งที่เรา Streaming มาให้เป็น String
+            try {
+
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+                StringBuilder objStringBuilder = new StringBuilder();   // ตัวที่ทำหน้าที่รวม
+                String strLine = null;  // ตัวแปรที่รับตัวที่ถูกตัดมา
+
+                while ((strLine = objBufferedReader.readLine())!= null ) {  // ถ้า strLine ว่างเปล่า ก็ออกจาก Loop
+
+                    objStringBuilder.append(strLine);   // มีหน้าที่คอยผูก String ไปเรื่อย ๆ
+
+
+                }   // While Loop
+                objInputStream.close();                 // ถ้าหมด ก็ไม่ต้องโหลดต่อ
+                strJSON = objStringBuilder.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d("Rest", "strJSON ==> "+e.toString());
+
+            }
+
+
+
+            // ข้้อที่ 3. Update SQLite     เอา strJSON ที่ได้มา มาใส่ใน SQLite
+            try {
+
+                final JSONArray objJsonArray = new JSONArray(strJSON);
+
+                for (int i = 0; i < objJsonArray.length(); i++) {
+
+                    JSONObject object = objJsonArray.getJSONObject(i);  // เอา i มาแทนค่าตำแหน่งของ Array
+
+                    switch (intTimes) {
+
+                        // สำหรับ UserTABLE
+                        // ได้ String 3 ตัวสำหรับใส่ใน DB แล้ว
+                        case 0:
+                        String strUser = object.getString("User");  // User เป็น Key ใน JSON
+                        String strPassword = object.getString("Password");
+                        String strName = object.getString("Name");
+                        String strAge = object.getString("Age");
+                        String strSex = object.getString("Sex");
+                        String strWeight = object.getString("Weight");
+                        String strHeight = object.getString("Height");
+                        String strEmail = object.getString("Email");
+
+                        objUserTABLE.addNewUser(strUser, strPassword, strName, strAge, strSex, strWeight, strHeight,strEmail);
+                            break;
+
+
+
+
+                    }
+
+                }   // วิ่งวนตามจำนวน แถวใน JSON
+
+            } catch (Exception e) {
+
+                Log.d("Rest", "Update Error ==> "+e.toString());
+
+            }
+
+            intTimes += 1;  // บวกทีละ 1
+
+        }   // while
+
+
+
+
+
+    } //synJSONtoSQLite
+
+    public void clickSignIn(View view) {
+        String strUser = userEditText.getText().toString().trim();
+        String strPassword = passwordEditText.getText().toString().trim();
+
+        //Check Zero
+        if (strUser.equals("") || strPassword.equals("") ) {
+
+            //Have Space
+            MyDialog objMyDialog = new MyDialog();
+            objMyDialog.errorDialog(MainActivity.this, "มีช่องว่าง", "กรุณากรอกให้ครบ");
+        } else {
+
+            //No Space
+
+        }
+
+
+
+    } // clickSignIn
+
+
     public void clickSignUp(View view) {
         startActivity(new Intent(this, SignUpActivity.class));
-    }
+    } // clickSignUp
 
     private void testAddValue() {
 
